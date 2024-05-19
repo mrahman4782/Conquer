86dea91c6f962c8e849521f8f0853d22d09008c1
mrahman4782 <moharahman50@gmail.com>
2024-05-19 06:07:52 -0400

    Middleware configuration

---
+++ server/middleware/config.js
@@ -0,0 +1,40 @@
+import cors from 'cors';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url'; 
+import express from 'express'; 
+
+// Get the directory name from the current module URL
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Initialize middleware setup
+export const middlewareInit = (app) => {
+    app.use(cors());
+    app.use(express.json());
+}
+
+// Setup log directory
+const logDir = path.join(__dirname, 'logs');
+
+// Check if log directory exists and create if not
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+// Logger to capture all requests to server
+export const logRequests = morgan('combined', {
+    stream: fs.createWriteStream(path.join(logDir, 'requests.log'), { flags: 'a' })
+})
+
+// Logger to capture valid responses from the server
+export const logSuccess = morgan('combined', {
+    skip: (req, res) => res.statusCode >= 400,
+    stream: fs.createWriteStream(path.join(logDir, 'responses.log'), { flags: 'a' })
+})
+
+// Logger to capture error responses from the server
+export const logFailure = morgan('combined', {
+    skip: (req, res) => res.statusCode < 400,
+    stream: fs.createWriteStream(path.join(logDir, 'errors.log'), { flags: 'a' })
+})
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from "dotenv";
+import { middlewareInit, logRequests, logSuccess, logFailure } from './middleware/config.js';
+
+dotenv.config({ path: "./.env" });
 
 import initializeFirebaseApp from './functions/firebaseInit.js';
-initializeFirebaseApp();
+initializeFirebaseApp(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 import {createUser} from './functions/register.js';
 import {loginVerify} from './functions/loginVerify.js';
@@ -10,8 +14,12 @@ import {loginVerify} from './functions/loginVerify.js';
 
 const app = express();
 const port = 5000;
-app.use(cors());
 
+middlewareInit(app);
+app.use(logRequests);
+app.use(logSuccess);
+app.use(logFailure);
+app.use(cors()); 
 app.use(express.json());
 
 // GET Requests
