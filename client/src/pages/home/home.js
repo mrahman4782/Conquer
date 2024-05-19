e8e8a60c16ba1014c9fbf490becf145edc728d6f
amimul-arnab <102828836+amimul-arnab@users.noreply.github.com>
2024-05-19 06:49:09 +0000

    fix

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './home.css';
 import profileIcon from './assets/profile.png';
 import photoIcon from './assets/photo.png';
